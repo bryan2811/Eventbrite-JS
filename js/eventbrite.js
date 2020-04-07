c7874f337bedbd79698d0b46b1dea0fb16cc653f
Bryan Silva <brandonb2816@gmail.com>
2020-04-07 19:33:35 -0300

    Creando la aplicación y las classes

---
+++ js/eventbrite.js
@@ -0,0 +1,12 @@
+class EventBrite {
+    constructor() {
+        this.token_auth = 'B7GEKAL4VDA4RQLZP43S';
+        this.ordenar = 'date';
+    }
+
+    // Obtiene las categorias en init()
+    async obtenerCategorias() {
+        // Consultar las categorias a la REST API
+        
+    }
+}

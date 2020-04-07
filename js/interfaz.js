c2de5803f5dcda306afb53926265598e26bf1adf
Bryan Silva <brandonb2816@gmail.com>
2020-04-07 19:49:33 -0300

    Leyendo las categorias de los eventos desde la REST API e insertandolas en el option

--- js/interfaz.js
+++ js/interfaz.js
@@ -8,11 +8,26 @@ class Interfaz {
 
     //Método para cuando inicialice la app
     init() {
-
+        // Llamar a imprimirCategorias()
+        this.imprimirCategorias();
     }
 
     // Imprimir categorias
     imprimirCategorias() {
-        const listaCategorias = eventbrite.obtenerCategorias();
+        const listaCategorias = eventbrite.obtenerCategorias()
+            .then(categorias => {
+                const cats = categorias.categorias.categories;
+                
+                // Seleccionar el Select de Categorias
+                const selectCategoria = document.getElementById('listado-categorias');
+
+                // Recorremos el arreglo e imprimimos los <option>
+                cats.forEach(cat => {
+                    const option = document.createElement('option');
+                    option.value = cat.id;
+                    option.appendChild(document.createTextNode(cat.name_localized));
+                    selectCategoria.appendChild(option);
+                })
+            })
     }
 }
